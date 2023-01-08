d848452bdc5353df4ca6c58ccd4d72a0a46188b3
Ljime <Ljime@users.noreply.github.com>
2023-01-08 18:18:01 -0500

    minor changes

--- client/src/layout/Main.js
+++ client/src/layout/Main.js
@@ -1,7 +1,6 @@
 import classes from './Main.module.scss'
 import { forwardRef } from 'react'
-import Label from '../components/Label'
-import resume from '../assets/resume.pdf'
+
 const Main = forwardRef((props,ref) => {
     return (
         <section ref={ref} className={classes.main}>
@@ -12,7 +11,6 @@ const Main = forwardRef((props,ref) => {
                 <h2 className={`${classes.type} ${classes.type2}`}>
                     Thanks for checking my website out!
                 </h2>
-                <Label className={classes.resume}><a target = "_blank" rel='noreferrer' href={resume}>Resume</a></Label>
             </div>
         </section>
     )
